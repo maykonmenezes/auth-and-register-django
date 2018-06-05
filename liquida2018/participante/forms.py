15b133fc6a61f58baaf64bbe0e17fc602f1e76d5
Maykon Menezes <maykon.meneses@gmail.com>
2018-06-05 10:46:13 -0300

    alterações de layout

--- liquida2018/participante/forms.py
+++ liquida2018/participante/forms.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 
+
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
@@ -13,10 +14,13 @@ class LoginForm(forms.Form):
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Senha', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Senha novamente', widget=forms.PasswordInput)
-
+    username = forms.CharField(label='Usuario')
+    first_name = forms.CharField(label='Nome Completo')
+    email = forms.CharField(label='Email')
     class Meta:
         model = User
         fields = ('username', 'first_name', 'email')
+
 
     def clean_password2(self):
         cd = self.cleaned_data
@@ -26,12 +30,17 @@ class UserRegistrationForm(forms.ModelForm):
 
 
 class UserEditForm(forms.ModelForm):
+    first_name = forms.CharField(label='Nome Completo')
+    last_name = forms.CharField(label='Sobrenome')
+    email = forms.CharField(label='Email')
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
 
 
 class ProfileEditForm(forms.ModelForm):
+    date_of_birth = forms.DateField(label='Data de Nascimento')
+    photo = forms.ImageField(label='Foto')
     class Meta:
         model = Profile
         fields = ('date_of_birth', 'photo')
