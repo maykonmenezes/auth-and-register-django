da173532373eaa455725bf42f9d4da293cebaa28
Maykon Menezes <maykon.meneses@gmail.com>
2018-06-13 08:52:40 -0300

    alterações nos redirecionamentos de urls

--- liquida2018/liquida2018/urls.py
+++ liquida2018/liquida2018/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
 urlpatterns = [
     url(r'^$', include('participante.urls')),
     url(r'^admin/', include(admin.site.urls)),
